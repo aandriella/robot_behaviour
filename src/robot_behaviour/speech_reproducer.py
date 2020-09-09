e4b74402adf70327b8e0560ce4241bad22f38f08
Antonio Andriella <aandriella@iri.upc.edu>
2020-09-09 18:05:42 +0200

    Found the way to cancel an action

--- src/robot_behaviour/speech_reproducer.py
+++ src/robot_behaviour/speech_reproducer.py
@@ -11,12 +11,13 @@ from actionlib_msgs.msg import GoalStatus
 class Speech():
   def __init__(self, language):
     rospy.init_node('big_hero', anonymous=True)
-    self.client = actionlib.SimpleActionClient('/tts_to_soundplay', TtsAction)
+    #change it to tts if you're using from the tiago
+    #TODO change the name of the action to tts
+    self.client = actionlib.SimpleActionClient('/tts', TtsAction)
     self.sound_client = SoundClient()
     self.language = language
     self.feedback = 0
     self.text = ""
-
   def event(self, event):
     #print("event {}".format(event))
     return {
@@ -32,13 +33,11 @@ class Speech():
     }[event]
 
   def feedbackCb(self, feedback):
-    # print("event type: " + self.event(feedback.event_type))
-    # print("timestamp: " + str(feedback.timestamp))
-    # print("current word: " + feedback.text_said)
-    # print("next word: " + feedback.next_word)
-    # print("-")
-    self.feedback = self.event(feedback.event_type)
-    return self.feedback
+    print("event type: " + self.event(feedback.event_type))
+    print("timestamp: " + str(feedback.timestamp))
+    print("current word: " + feedback.text_said)
+    print("next word: " + feedback.next_word)
+    print("-")
 
   def text_to_speech(self, text, locked=False, threshold=10):
     #rospy.loginfo("Waiting for Server")
@@ -48,7 +47,9 @@ class Speech():
     goal.rawtext.text = text
     length = len(text)
     goal.rawtext.lang_id = self.language
+    t0 = time.time()
     self.client.send_goal(goal, feedback_cb=self.feedbackCb)
+
     goal_state = self.client.get_state()
     if locked:
       self.client.wait_for_result(timeout=rospy.Duration(5))
@@ -57,12 +58,11 @@ class Speech():
     if goal_state != GoalStatus.SUCCEEDED:
       self.client.stop_tracking_goal()
 
-
-    return length/threshold
-
+    return
+  
   def cancel_reproduction(self):
     rospy.loginfo("canceling...")
-    self.sound_client.stopAll()
+    self.text_to_speech("", False)
     rospy.loginfo("goal has been canceled")
 
   def get_status(self):
@@ -71,17 +71,20 @@ class Speech():
 #
 if __name__ == "__main__":
     speech = Speech("en_GB")
-    text = "This is the sentence you have to reproduce"
+    text = "This is the sentence you have to reproduce, for istance."
     start = time.time()
-    time_to_reproduce = speech.text_to_speech("111", True, 1)
-    rospy.sleep(time_to_reproduce)
+    t = speech.text_to_speech(text, False, 1)
+    time_to_reproduce = 5
+    print(time_to_reproduce)
+    # rospy.sleep(time_to_reproduce)
     elapsed_time = 0
-    print("len:", time_to_reproduce)
+    # print("len:", time_to_reproduce)
     while(elapsed_time<time_to_reproduce):
       elapsed_time = time.time()-start
-      print("wait")
-    speech.cancel_reproduction()
-
+      if elapsed_time>2:
+        speech.cancel_reproduction()
+        print("speech", speech.client.get_state())
+        break
     #success_against = speech.text_to_speech("test again agaist you", True)
 
     # while(elapsed_time<4):
