ce7ee36959c1deb5f4aaa75514fd29962ca8efad
Antonio Andriella <aandriella@iri.upc.edu>
2021-09-28 18:22:12 +0200

    Add audio which include your_turn

---
+++ src/robot_behaviour/config/audio/merge_audio_male_challenge.sh
@@ -0,0 +1,22 @@
+cd /home/pal/carf_ws/src/robot_behaviour/src/robot_behaviour/config/audio/female/challenge/esp
+
+sox G.wav your_turn.wav G_.wav
+sox O.wav your_turn.wav O_.wav
+sox D.wav your_turn.wav D_.wav
+sox I.wav your_turn.wav I_.wav
+sox R.wav your_turn.wav R_.wav
+sox L.wav your_turn.wav L_.wav
+sox M.wav your_turn.wav M_.wav
+sox E.wav your_turn.wav E_.wav
+sox S.wav your_turn.wav S_.wav
+sox U.wav your_turn.wav U_.wav
+
+sox Lev_1_1.wav your_turn.wav Lev_1_1_.wav
+sox Lev_1_2.wav your_turn.wav Lev_1_2_.wav
+sox Lev_1_3.wav your_turn.wav Lev_1_3_.wav
+sox Lev_2_center.wav your_turn.wav Lev_2_center_.wav
+sox Lev_2_left.wav your_turn.wav Lev_2_left_.wav
+sox Lev_2_right.wav your_turn.wav Lev_2_right_.wav
+
+
+
